0ec6e23aec4d8e26ba6453e7dfe57bdec5ffcaec
hyun-hyang <jimin3060@naver.com>
2022-09-29 20:23:29 +0900

    meta 수정

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,14 +7,14 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'blog',
+    title: 'hyun-hyang',
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: '현향, 임지민의 블로그입니다.' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
