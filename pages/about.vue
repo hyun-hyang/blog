40409f5356bb99e67a41fa2b4dfd8ebc87e738c2
hyun-hyang <jimin3060@naver.com>
2022-11-03 21:48:55 +0900

    about page updated

--- pages/about.vue
+++ pages/about.vue
@@ -1,3 +1,35 @@
 <template>
-    
+    <div class="max-w-6xl mx-auto">
+     <!--나 소개-->
+     <div class="pt-16">
+      <div class="pt-14 md:pt-36 pb-0 md:pb-10 max-w-6xl mx-auto px-6">
+        <p><img src="https://capsule-render.vercel.app/api?type=waving&amp;color=auto&amp;height=300&amp;section=header&amp;text=welcome&amp;fontSize=90&amp;animation=fadeIn&amp;fontAlignY=38&amp;desc=Ji-Min,%20Lim&#39;s%20GitHub%20Profile&amp;descAlignY=51&amp;descAlign=62" alt="header"></p>
+        <h3 id="student">Student</h3>
+        <ul>
+            <li>studying at Chungang University, Bachelor of Computer Science and Engineering (Seoul,South Korea, Mar.2021~Present)</li>
+            <li>Vice President of a club - <a href="https://zp-portal.org/">ZeroPage</a>, Chung-Ang University Software Department Academic Research Association</li>
+        </ul>
+        <h3 id="interest">Interest</h3>
+        <ul>
+            <li>international experience (due to study abroad in student exchange program, 2023-)</li>
+            <li>OOP(Object-Oriented Programming) : Java, C++</li>
+            <li>Reading &amp; Writing</li>
+        </ul>
+        <div align=center>
+            <a href="https://hits.seeyoufarm.com"><img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fhyun-hyang%2Fhit-counter&amp;count_bg=%238351C2&amp;title_bg=%black&amp;title=visitors&amp;edge_flat=true" alt="Hits"></a> &nbsp;
+            <a href="https://www.hyun-hyang.com/"><img src="http://img.shields.io/badge/-Personal%20blog-ff69b4?style=social&amp;logo=4chan&amp;logoColor=black&amp;link=https://www.hyun-hyang.com/" alt="Personal Blog Badge"></a> &nbsp;
+            <a href="mailto:jylim3060@gmail.com"><img src="https://img.shields.io/badge/Gmail-d14836?style=flat-square&amp;logo=Gmail&amp;logoColor=white&amp;link=mailto:jylim3060@gmail.com" alt="Gmail Badge"></a>
+        </div>
+      </div>
+    </div>
+    </div>
 </template>
+
+<!--[header](https://capsule-render.vercel.app/api?type=waving&color=auto&height=300&section=header&text=welcome&fontSize=90&animation=fadeIn&fontAlignY=38&desc=Ji-Min,%20Lim's%20GitHub%20Profile&descAlignY=51&descAlign=62)
+          <h2 class="pb-6 poppins text-left md:text-left text-4xl md:text-6xl font-extrabold text-gray-800 font-title">
+            Hello world, I'm <span class="highlight-sm font-title">Hyun-hyang</span> !
+          </h2>
+          <div class="font-normal text-sm md:text-base text-gray-600 keepall">
+            나의 기록😾들을 담는 곳, <br> 주로 CS와 관련된 내용을 다룹니다.
+          </div>
+        -->
