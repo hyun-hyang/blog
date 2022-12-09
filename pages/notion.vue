11768f8add87f2f29c97b516a29ce93011f2a2d9
hyun-hyang <jimin3060@naver.com>
2022-12-09 17:33:12 +0900

    Added : notion

---
+++ pages/notion.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="max-w-6xl mx-auto">
+        <div class="pt-16">
+            <div class="pt-14 md:pt-36 pb-0 md:pb-10 max-w-6xl mx-auto px-6">
+
+            </div>
+        </div>
+    </div>
+</template>
